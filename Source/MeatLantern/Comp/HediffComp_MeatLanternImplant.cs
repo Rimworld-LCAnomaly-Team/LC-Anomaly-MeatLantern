32d3eed577ed621180b8eb0e5a1fcc05f7f1e5a1
DarthCY <452710557@qq.com>
2024-05-16 01:19:01 +0800

    add：肉食提灯植入物（1.8/2f）
    特效

--- Source/MeatLantern/Comp/HediffComp_MeatLanternImplant.cs
+++ Source/MeatLantern/Comp/HediffComp_MeatLanternImplant.cs
@@ -1,11 +1,14 @@
 ﻿using RimWorld;
 using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
 using Verse;
 
 namespace MeatLantern.Comp
 {
     public class HediffComp_MeatLanternImplant : HediffComp
     {
+        private StringBuilder sb_MoteVampire;
 
         public HediffCompProperties_MeatLanternImplant Props => (HediffCompProperties_MeatLanternImplant)props;
 
@@ -17,6 +20,12 @@ namespace MeatLantern.Comp
         public override void Notify_SurgicallyReplaced(Pawn surgeon)
         {
 
+        }
+
+        public override void Notify_Spawned()
+        {
+            base.Notify_Spawned();
+            sb_MoteVampire = new StringBuilder();
         }
 
         /// <summary>
@@ -35,13 +44,29 @@ namespace MeatLantern.Comp
         /// <param name="healAmount">治愈比率</param>
         private void HealSelfPawn(Pawn selfPawn, float healAmount, float percent)
         {
-            Log.Message($"吸血咯，healAmount = {healAmount}，percent = {percent}");
-
             List<Hediff_Injury> list = new List<Hediff_Injury>();
             selfPawn.health.hediffSet.GetHediffs(ref list);
 
-            if(list.Count > 0 )
-                list.RandomElement().Heal(healAmount * percent);
+            if(list.Count > 0)
+            {
+                Hediff_Injury injury = list.RandomElement();
+                injury.Heal(healAmount * percent);
+
+                //TODO 配置文件：允许显示吸血字样
+                if (true)
+                {
+                    sb_MoteVampire.Clear();
+                    sb_MoteVampire.Append($"吸血：{healAmount * percent}");
+
+                    //TODO 配置文件：允许显示部位
+                    if (true)
+                        sb_MoteVampire.Append($"，部位是：{injury.Part.Label.Translate()}");
+
+                    MoteMaker.ThrowText(selfPawn.Position.ToVector3(), selfPawn.Map, sb_MoteVampire.ToString(), Color.green);
+                }
+                FleckMaker.ThrowMetaIcon(selfPawn.Position, selfPawn.Map, FleckDefOf.HealingCross, 0.42f);
+            }
+                
         }
     }
 }
