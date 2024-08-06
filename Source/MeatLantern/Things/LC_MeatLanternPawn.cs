1b26afe4cd47f3bc5c106f457002f07591dc9ab5
DarthCY-K <452710557@qq.com>
2024-08-06 19:25:50 +0800

    Create PublishedFileId.txt

--- Source/MeatLantern/Things/LC_MeatLanternPawn.cs
+++ Source/MeatLantern/Things/LC_MeatLanternPawn.cs
@@ -12,10 +12,12 @@ namespace MeatLantern.Things
         public override void Tick()
         {
             //收容状态下丢下就出逃
-            if(CarriedBy == null && kindDef == Def.PawnKindDefOf.MeatLanternContained)
+            if (CarriedBy == null && kindDef == Def.PawnKindDefOf.MeatLanternContained)
             {
                 GetComp<CompMeatLantern>()?.Notify_Escaped();
             }
+
+            base.Tick();
         }
     }
 }
